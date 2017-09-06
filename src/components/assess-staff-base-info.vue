04f5b7348cafea79a2e603376640fcf92685772b
Zhou <zhijun.zhou@hpe.com>
2017-09-06 13:23:28 +0800

    add staff base info components

---
+++ src/components/assess-staff-base-info.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="item item-avatar">
+        <img src="mcfly.jpg">
+        <div>{{staffName}} &nbsp;&nbsp;<Tags type="border" color="blue" style="">
+                晋级
+            </Tags>
+        </div>
+        <Rate v-model="remarkRate" disabled style="font-size:0.32rem"></Rate>
+        <span style="
+            position: relative;
+            top: 0.05rem;
+            right: 0.10rem;
+            font-size: 0.10rem;
+            ">
+            {{remarkName}}  
+            
+            </span>
+        <Tags type="border" color="blue" style="
+            position:absolute; 
+            top:0.54rem; 
+            right: 0.40rem; 
+            ">
+            <i class="icon ion-reply"></i> 驳回
+        </Tags>
+      </div>
+</template>
+<script>
+    import Tags from '../components/tag.vue';
+    
+    export default {
+        name: 'AssessStaffBaseInfo',
+        props: ['staffName', 'remarkName', 'remarkRate'],
+        components: {
+            Tags
+        }
+    }
+</script>
--- src/views/p071.vue
+++ src/views/p071.vue
@@ -1,124 +1,7 @@
-<style scoped>
-.badge{
-    font-weight: initial;
-    padding: 0.06rem 0.14rem;
-}
-
-#index{
-    height: -webkit-fill-available;
-    overflow-y: auto;
-    background-color: #f5f5f5;
-}
-
-.highlightdeadLineColor{
-    color: #ff8661;
-}
-.reminderColor{
-    color: #919191;
-    font-size:0.24rem;
-}
-
-.card{
-    margin:0.20rem 0.22rem;
-/*    height: 1.40rem;*/
-    box-shadow:none;
-    border-radius:10px;
-    overflow:auto;
-}
-
-.card .item{
-    height: auto;
-/*    padding-top: 0.30em;
-    padding-left: 0.40em;*/
-}
-
-.list{
-    padding: 0;
-}
-
-.list .item{
-    border-width:0px 0px 1px 0px;
-}
-
-.ivu-tag-text{
-    position: absolute;
-    bottom: 13px;
-}
-
-.titlescss{
-    font-size:0.32rem;
-    color: #242424;
-    position:absolute; 
-    left:0.43rem
-}
-
-.morebar{
-
-    height: 0.90rem;
-    padding: 0.23rem 0 0 3.10rem;
-}
-
-.panel-title{
-    padding: 0 0 0 0.3rem;
-}
-
-.box{ float: left; padding: 0 0.3rem 0.2rem 0.1rem;}
-
-.font-level1-title{font:32px; color: #3f3f3f;}
-.font-level2-title{font:26px; color: #5e5e5e;}
-.font-level2-content{font:26px;color:#919191;}
-.font-level2-description{font:26px;color:#acacac;}
-
-.clr{clear:both;}
-
-.ivu-input-number-input-wrap {
-    overflow: hidden;
-    height: 32px;
-}
-
-.ivu-input-number-input {
-    width: 100%;
-    height: 32px;
-    line-height: 32px;
-    padding: 0 7px;
-    text-align: left;
-    outline: 0;
-    -moz-appearance: textfield;
-    color: #666;
-    border: 0;
-    border-radius: 4px;
-    transition: all .2s linear;
-}
-
-</style>
 <template>
 <div id="index">
     <div class="list card">
-
-      <div class="item item-avatar">
-        <img src="mcfly.jpg">
-        <div>{{staffName}} &nbsp;&nbsp;<Tags type="border" color="blue" style="">
-                晋级
-            </Tags>
-        </div>
-        <Rate v-model="remarkRate" disabled style="font-size:0.32rem"></Rate>
-        <span style="
-            position: relative;
-            top: 0.05rem;
-            right: 0.10rem;
-            font-size: 0.10rem;
-            ">
-            {{remarkName}}  
-            
-            </span>
-        <Tags type="border" color="blue" style="
-            position:absolute; 
-            top:0.54rem; 
-            right: 0.40rem; 
-            ">
-            <i class="icon ion-reply"></i> 驳回
-        </Tags>
-      </div>
+        <assess-staff-base-info :staffName="staffName" :remarkName="remarkName" :remarkRate="remarkRate"></assess-staff-base-info>
     </div>
     <div class="panel-title font-level1-title">
         领导力行为评估
@@ -364,10 +247,12 @@
     import Util from '../libs/util.js';
     import {STEPNAME,TAGNAME,EVALUATEDNAME,AssessLevelID_RATE,getValue} from '../constants.js';
     import Tags from '../components/tag.vue';
+    import AssessStaffBaseInfo from '../components/assess-staff-base-info'
 
     export default {
         data () {
             return {
+                data: undefined,
                 xiangGuanPingGuDaiQuanZhongCard:[],
                 zhongHePingGuCard:{},
                 shangJiPingJiaResult:'',
@@ -398,7 +283,8 @@
             }
         },
         components: {
-            Tags
+            Tags,
+            AssessStaffBaseInfo
         },
         methods: {
             getData () {
@@ -413,6 +299,10 @@
                 })
                 let res = this.API.result.Data;
                 this.submitData = this.API.result.Data;
+
+                // assign root data
+                this.data = this.API.result.Data;
+
                 this.staffName = res.AssessStaffBaseInfoQueryResult.StaffName;
                 this.remarkRate = 0;
                 this.remarkName = '';
@@ -500,3 +390,96 @@
         }
     };
 </script>
+<style scoped>
+.badge{
+    font-weight: initial;
+    padding: 0.06rem 0.14rem;
+}
+
+#index{
+    height: -webkit-fill-available;
+    overflow-y: auto;
+    background-color: #f5f5f5;
+}
+
+.highlightdeadLineColor{
+    color: #ff8661;
+}
+.reminderColor{
+    color: #919191;
+    font-size:0.24rem;
+}
+
+.card{
+    margin:0.20rem 0.22rem;
+/*    height: 1.40rem;*/
+    box-shadow:none;
+    border-radius:10px;
+    overflow:auto;
+}
+
+.card .item{
+    height: auto;
+/*    padding-top: 0.30em;
+    padding-left: 0.40em;*/
+}
+
+.list{
+    padding: 0;
+}
+
+.list .item{
+    border-width:0px 0px 1px 0px;
+}
+
+.ivu-tag-text{
+    position: absolute;
+    bottom: 13px;
+}
+
+.titlescss{
+    font-size:0.32rem;
+    color: #242424;
+    position:absolute; 
+    left:0.43rem
+}
+
+.morebar{
+
+    height: 0.90rem;
+    padding: 0.23rem 0 0 3.10rem;
+}
+
+.panel-title{
+    padding: 0 0 0 0.3rem;
+}
+
+.box{ float: left; padding: 0 0.3rem 0.2rem 0.1rem;}
+
+.font-level1-title{font:32px; color: #3f3f3f;}
+.font-level2-title{font:26px; color: #5e5e5e;}
+.font-level2-content{font:26px;color:#919191;}
+.font-level2-description{font:26px;color:#acacac;}
+
+.clr{clear:both;}
+
+.ivu-input-number-input-wrap {
+    overflow: hidden;
+    height: 32px;
+}
+
+.ivu-input-number-input {
+    width: 100%;
+    height: 32px;
+    line-height: 32px;
+    padding: 0 7px;
+    text-align: left;
+    outline: 0;
+    -moz-appearance: textfield;
+    color: #666;
+    border: 0;
+    border-radius: 4px;
+    transition: all .2s linear;
+}
+
+</style>
