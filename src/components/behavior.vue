92e6ab0c50f844381032cd26f5b884927fffe99d
zibei.liu <251107094@qq.com>
2017-09-06 15:43:23 +0800

    add a new compoment behavior-item from behavior

--- src/components/behavior.vue
+++ src/components/behavior.vue
@@ -7,23 +7,19 @@
 							<div class="col col-30" style="padding: 0;text-align: right;">上级评价</div>
 					</div>						
 			</li>
-			<li class="item"  style="height: 1.00rem;margin:0;" v-for="lst in XingWeiPingGuList">
-					<div class="row" style="padding: 0">
-							<div class="col col-30" style="padding: 0">{{lst.DimensionName}}</div>
-							<div class="col col-40" style="padding: 0">{{lst.selfScoreName}}</div>
-							<div class="col col-30" style="padding: 0;text-align: right;">
-									<select v-model="lst.shangJiPingJiaResult" @change="setShangJiPingJia(lst.SortNum,lst.shangJiPingJiaResult)" >
-											<option v-for="opt in commentList" :value="opt.Score" :key="opt.Score">{{opt.ScoreName}}</option>
-									</select>
-							</div>
-					</div>
+			<li class="item"  style="height: 1.00rem;margin:0;" v-for="lst in XingWeiPingGuList">					
+				<behavior-item :lst='lst' :commentList='commentList' :setShangJiPingJia="setShangJiPingJia"></behavior-item>
 			</li>
 	</ul>
 </template>
-<script>    
+<script>
+	import BehaviorItem from '../components/behavior-item';
+
     export default {
         name: 'behavior',
         props: ['commentList', 'XingWeiPingGuList', 'setShangJiPingJia'],
-        components: {}
+        components: {
+        	BehaviorItem
+        }
     }
 </script>
