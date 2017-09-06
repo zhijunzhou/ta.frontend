bee8612f1227c1b35f70c8024364d241217bac27
Zhou <zhijun.zhou@hpe.com>
2017-09-06 13:37:29 +0800

    add behavior components

---
+++ src/components/behavior.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul class="list">
+			<li class="item"  style="height: 1.00rem;margin:0;" >
+					<div class="row" style="padding: 0">
+							<div class="col col-30" style="padding: 0">评估维度</div>
+							<div class="col col-40" style="padding: 0">行为自评 </div>
+							<div class="col col-30" style="padding: 0;text-align: right;">上级评价</div>
+					</div>						
+			</li>
+			<li class="item"  style="height: 1.00rem;margin:0;" v-for="lst in XingWeiPingGuList">
+					<div class="row" style="padding: 0">
+							<div class="col col-30" style="padding: 0">{{lst.DimensionName}}</div>
+							<div class="col col-40" style="padding: 0">{{lst.selfScoreName}}</div>
+							<div class="col col-30" style="padding: 0;text-align: right;">
+									<select v-model="lst.shangJiPingJiaResult" @change="setShangJiPingJia(lst.SortNum,lst.shangJiPingJiaResult)" >
+											<option v-for="opt in commentList" :value="opt.Score" :key="opt.Score">{{opt.ScoreName}}</option>
+									</select>
+							</div>
+					</div>
+			</li>
+	</ul>
+</template>
+<script>    
+    export default {
+        name: 'behavior',
+        props: ['commentList', 'XingWeiPingGuList', 'setShangJiPingJia'],
+        components: {}
+    }
+</script>
--- src/views/p071.vue
+++ src/views/p071.vue
@@ -7,27 +7,7 @@
         领导力行为评估
     </div>
     <div class="list card">
-        <ul class="list">
-            <li class="item"  style="height: 1.00rem;margin:0;" >
-                <div class="row" style="padding: 0">
-                    <div class="col col-30" style="padding: 0">评估维度</div>
-                    <div class="col col-40" style="padding: 0">行为自评 </div>
-                    <div class="col col-30" style="padding: 0;text-align: right;">上级评价</div>
-                </div>
-                 
-            </li>
-            <li class="item"  style="height: 1.00rem;margin:0;" v-for="lst in XingWeiPingGuList">
-                <div class="row" style="padding: 0">
-                    <div class="col col-30" style="padding: 0">{{lst.DimensionName}}</div>
-                    <div class="col col-40" style="padding: 0">{{lst.selfScoreName}}</div>
-                    <div class="col col-30" style="padding: 0;text-align: right;">
-                        <select v-model="lst.shangJiPingJiaResult" @change="setShangJiPingJia(lst.SortNum,lst.shangJiPingJiaResult)" >
-                            <option v-for="opt in commentList" :value="opt.Score" :key="opt.Score">{{opt.ScoreName}}</option>
-                        </select>
-                    </div>
-                </div>
-            </li>
-        </ul>
+        <behavior :XingWeiPingGuList="XingWeiPingGuList" :commentList="commentList" :setShangJiPingJia="setShangJiPingJia"></behavior>
     </div>
     <div class="list card">
 
@@ -248,6 +228,7 @@
     import {STEPNAME,TAGNAME,EVALUATEDNAME,AssessLevelID_RATE,getValue} from '../constants.js';
     import Tags from '../components/tag.vue';
     import AssessStaffBaseInfo from '../components/assess-staff-base-info'
+    import Behavior from '../components/behavior'
 
     export default {
         data () {
@@ -284,7 +265,8 @@
         },
         components: {
             Tags,
-            AssessStaffBaseInfo
+            AssessStaffBaseInfo,
+            Behavior
         },
         methods: {
             getData () {
