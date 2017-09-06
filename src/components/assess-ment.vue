a5b2de06e7273624e10869b6e747c9facfa7e177
Zhou <zhijun.zhou@hpe.com>
2017-09-06 20:15:30 +0800

    split assess ments

--- src/components/assess-ment.vue
+++ src/components/assess-ment.vue
@@ -1,68 +1,19 @@
 <template>
-	<div class="item item-body" style="height: auto" v-if="zhongHePingGuCard">
-        <div>
-            <div class="box">
-                <div style="float:left;">
-                    <span>评分:</span>
-                </div>
-                <div class="ivu-input-number-input-wrap" style="float:left; padding: 0 0 0 0.2rem;">
-                    <input type="number" class="ivu-input-number-input" name="pingFen" v-model="zhongHePingGuCard.DirectTotalScore">
-                </div>                
-            </div>
-            <div class="box">
-                <div style="float:left;">
-                    <span>权重:</span>
-                </div>
-                <div style="float:left; padding: 0 0 0 0.2rem;">
-                    <span>{{zhongHePingGuCard.QuanZhong + "%"}}</span>
-                </div>
-            </div>
-            <p class="clr"> </p>
-        </div>
-        <div>
-            <div class="box">
-                <div style="float:left;">
-                    <span>综合评分:</span>
-                </div>
-                <div style="float:left; padding: 0 0 0 0.2rem;">
-                    <span class="font-level2-content">{{zhongHePingGuCard.SumTotal}}</span>
-                </div>
-            </div>
-            <p class="clr"> </p>
-        </div>
-        <div>
-            <div class="box">
-                <div style="float:left;">
-                    <span>综合评级:</span>
-                </div>
-                <div style="float:left; padding: 0 0 0 0.2rem;">
-                    <Rate v-model="zhongHePingGuCard.rateValue" ></Rate>
-                    <div>{{zhongHePingGuCard.AssessLevelName}}</div>
-                </div>
-            </div>
-            <p class="clr"> </p>
-        </div>
-        <div>
-            <div class="box">
-                <div style="float:left;">
-                    <span>综合评语:</span>
-                </div>
-                <div style="float:left; padding: 0 0 0 0.2rem;">
-                    <Input style="width: 4.0rem;" v-model="zhongHePingGuCard.Remark" type="textarea" :rows="4" placeholder="请输入...">
-                    </Input>
-                </div>
-            </div>
-            <p class="clr"> </p>
-        </div>
-        <div>{{zhongHePingGuCard.OwnerName}}</div>
-        <div>{{zhongHePingGuCard.ownerRemark}}</div>
-      </div>
+    <div class="item item-body" v-if="summaryData">        
+        <cur-leader-assess-ment :cardData="summaryData"></cur-leader-assess-ment>
+        <leader-assess-ments :cardData="summaryData"></leader-assess-ments>
+    </div>
 </template>
 <script>
-	export default {
-        name: 'AssessMent',
-        props: ['zhongHePingGuCard'],
-        components: {
-        }
+import CurLeaderAssessMent from './cur-leader-assess-ment'
+import LeaderAssessMents from './leader-assess-ments'
+
+export default {
+    name: 'AssessMent',
+    props: ['summaryData'],
+    components: {
+        CurLeaderAssessMent,
+        LeaderAssessMents
     }
+}
 </script>
---
+++ src/components/cur-leader-assess-ment.vue
@@ -0,0 +1,53 @@
+<template>
+	<div v-if="cardData">
+		<Row>
+			<Col span="6">
+				<span>评分：</span>
+			</Col>
+			<Col span="10">
+				<InputNumber :max="100" :min="1" v-model="cardData.DirectTotalScore"></InputNumber>
+			</Col>
+			<Col span="8">
+				<span>权重：</span>				
+				<span v-if="cardData.QuanZhong">{{cardData.QuanZhong + "%"}}</span>
+			</Col>
+		</Row>
+		<Row>
+			<Col span="6">
+				<span>综合评分：</span>
+			</Col>
+			<Col span="18">
+				<span>{{cardData.SumTotal}}</span>
+			</Col>
+		</Row>
+		<Row>
+			<Col span="6">
+				<span>综合评级：</span>
+			</Col>
+			<Col span="18">
+				<Rate v-model="cardData.rateValue" ></Rate>
+			</Col>
+		</Row>
+		<Row>
+			<Col span="6">
+				<span>综合评语：</span>
+			</Col>			
+			<Col span="18">
+					<Input v-model="cardData.Remark" type="textarea" :rows="4" placeholder="请输入...">
+						</Input>
+			</Col>
+		</Row>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'CurLeaderAssessMent',
+	props: ['cardData']
+}
+</script>
+<style scoped>
+#totalScore {
+	width: 20px;
+}
+</style>
