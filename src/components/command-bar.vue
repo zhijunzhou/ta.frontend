fed812bec99f1a3dbbd24a9fade059c592d22f35
zibei.liu <251107094@qq.com>
2017-09-06 17:32:15 +0800

    Add command-bar.vue component

---
+++ src/components/command-bar.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="bar bar-footer" style="height:70px;">
+        <div style="width:20%">
+            <div style="text-align:center;">
+                <a class="button button-icon icon ion-ios-keypad-outline"></a>
+            </div>
+            <div style="text-align:center; font-size:18px;">
+                评估
+            </div>
+        </div>
+        <div style="width:20%">
+            <div  style="text-align:center;">
+                <a class="button button-icon icon ion-settings"></a>
+            </div>
+            <div style="text-align:center;font-size:18px;">
+                看自评
+            </div>
+        </div>
+        <div style="width:20%">
+            <div style="text-align:center;">
+                <a class="button button-icon icon ion-settings"></a>
+            </div>
+            <div style="text-align:center;font-size:18px;">
+                看周边
+            </div>
+        </div>
+        <div style="width:20%; height: 70px; background: #33CC33; text-align: center; line-height:70px;">
+            <a style="color: white; font-size: 13px;}">存草稿</a>
+        </div>
+        <div style="width:20%;height:70px;background-color:#0066FF; text-align: center; line-height:70px;">
+            <a style="color: white; font-size: 13px;}">保存并返回</a>
+        </div>
+    </div>
+</template>
+<script>
+	export default{
+		name:'CommandBar',
+		props: [],
+		components:{
+
+		}
+	}
+</script>
--- src/views/p071.vue
+++ src/views/p071.vue
@@ -23,38 +23,7 @@
     <ppe></ppe>
     <div style="padding:0 0 50px 0">
     </div>
-    <div class="bar bar-footer" style="height:70px;">
-        <div style="width:20%">
-            <div style="text-align:center;">
-                <a class="button button-icon icon ion-ios-keypad-outline"></a>
-            </div>
-            <div style="text-align:center; font-size:18px;">
-                评估
-            </div>
-        </div>
-        <div style="width:20%">
-            <div  style="text-align:center;">
-                <a class="button button-icon icon ion-settings"></a>
-            </div>
-            <div style="text-align:center;font-size:18px;">
-                看自评
-            </div>
-        </div>
-        <div style="width:20%">
-            <div style="text-align:center;">
-                <a class="button button-icon icon ion-settings"></a>
-            </div>
-            <div style="text-align:center;font-size:18px;">
-                看周边
-            </div>
-        </div>
-        <div style="width:20%; height: 70px; background: #33CC33; text-align: center; line-height:70px;">
-            <a style="color: white; font-size: 13px;}">存草稿</a>
-        </div>
-        <div style="width:20%;height:70px;background-color:#0066FF; text-align: center; line-height:70px;">
-            <a style="color: white; font-size: 13px;}">保存并返回</a>
-        </div>
-    </div>
+    <command-bar></command-bar>
 </div>
 </template>
 <script>
@@ -67,6 +36,7 @@
     import DashedAssessMent from '../components/dashed-assess-ment'
     import AssessMent from '../components/assess-ment'
     import Ppe from '../components/ppe'
+    import CommandBar from '../components/command-bar'
 
     export default {
         data () {
@@ -107,7 +77,8 @@
             LeadershipSurvey,
             DashedAssessMent,
             AssessMent,
-            Ppe
+            Ppe,
+            CommandBar
         },
         methods: {
             getData () {
