69a30ba9bab93e8dff400e8296494e6a1c721a20
zibei.liu <251107094@qq.com>
2017-09-06 18:37:02 +0800

    change commandbar css

--- src/components/command-bar.vue
+++ src/components/command-bar.vue
@@ -1,34 +1,34 @@
 <template>
-	<div class="bar bar-footer" style="height:70px;">
-        <div style="width:20%">
-            <div style="text-align:center;">
+	<div class="bar bar-footer bar-height">
+        <div class="panel-width">
+            <div>
                 <a class="button button-icon icon ion-ios-keypad-outline"></a>
             </div>
-            <div style="text-align:center; font-size:18px;">
+            <div class="panel-content">
                 评估
             </div>
         </div>
-        <div style="width:20%">
-            <div  style="text-align:center;">
-                <a class="button button-icon icon ion-settings"></a>
+        <div class="panel-width">
+            <div>
+                <a class="button button-icon icon ion-ios-chatboxes-outline"></a>
             </div>
-            <div style="text-align:center;font-size:18px;">
+            <div class="panel-content">
                 看自评
             </div>
         </div>
-        <div style="width:20%">
-            <div style="text-align:center;">
-                <a class="button button-icon icon ion-settings"></a>
+        <div class="panel-width">
+            <div>
+                <a class="button button-icon icon ion-android-people"></a>
             </div>
-            <div style="text-align:center;font-size:18px;">
+            <div class="panel-content">
                 看周边
             </div>
         </div>
-        <div style="width:20%; height: 70px; background: #33CC33; text-align: center; line-height:70px;">
-            <a style="color: white; font-size: 13px;}">存草稿</a>
+        <div class="bar-height save-button save-button-draft-color panel-width">
+            <a class="save-button-font">存草稿</a>
         </div>
-        <div style="width:20%;height:70px;background-color:#0066FF; text-align: center; line-height:70px;">
-            <a style="color: white; font-size: 13px;}">保存并返回</a>
+        <div class="bar-height save-button save-button-save-color panel-width">
+            <a class="save-button-font">保存并返回</a>
         </div>
     </div>
 </template>
@@ -41,3 +41,32 @@
 		}
 	}
 </script>
+<style scoped>
+.bar-height{
+    height:70px;
+}
+.panel-width{
+	width:20%;
+}
+.panel-width div{
+	text-align:center;
+}
+.panel-content{
+	text-align:center;
+	font-size:18px;
+}
+.save-button{
+	text-align: center; 
+	line-height:70px;
+}
+.save-button-font{
+	color: white; 
+	font-size: 12px;
+}
+.save-button-draft-color{
+	background: #33CC33;
+}
+.save-button-save-color{
+	background: #0066FF;
+}
+</style>
