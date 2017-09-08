f2f672d3b6e08a5b00228c4875071d8ac3fc57b7
Zhou <zhijun.zhou@hpe.com>
2017-09-08 15:14:48 +0800

    gray text and iview Select

--- src/components/leadership-question.vue
+++ src/components/leadership-question.vue
@@ -2,9 +2,9 @@
     <ul class="list">
 		<li class="item">
 			<Row>
-				<Col span="8">评估维度</Col>
-				<Col span="8">行为自评</Col>
-				<Col span="8">上级评价</Col>
+				<Col span="8"><span class="ct-item-header">评估维度</span></Col>
+				<Col span="8"><span class="ct-item-header">行为自评</span></Col>
+				<Col span="8"><span class="ct-item-header">上级评价</span></Col>
 			</Row>					
 		</li>
 		<li class="item" v-for="(Dimension,index) in LeadershipContent.Dimensions"  v-bind:key="index" v-if="LeadershipContent && SelfLeadershipContent">	
@@ -23,3 +23,11 @@
         }
     }
 </script>
+<style scoped>
+.item {
+	overflow: visible;
+}
+.ct-item-header {
+	font-size: 0.22rem;
+}
+</style>
