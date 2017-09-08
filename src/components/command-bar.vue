078ce6a92de091ac0fd528591972ad0705a3743d
Zhou <zhijun.zhou@hpe.com>
2017-09-08 14:24:19 +0800

    update bottom bar layout and style

--- src/components/command-bar.vue
+++ src/components/command-bar.vue
@@ -1,35 +1,41 @@
 <template>
 	<div class="bar bar-footer bar-height">
-        <div class="panel-width">
-            <div>
-                <a class="button button-icon icon ion-ios-keypad-outline"></a>
-            </div>
-            <div class="panel-content">
-                评估
-            </div>
-        </div>
-        <div class="panel-width">
-            <div>
-                <a class="button button-icon icon ion-ios-chatboxes-outline"></a>
-            </div>
-            <div class="panel-content">
-                看自评
-            </div>
-        </div>
-        <div class="panel-width">
-            <div>
-                <a class="button button-icon icon ion-android-people"></a>
-            </div>
-            <div class="panel-content">
-                看周边
-            </div>
-        </div>
-        <div class="bar-height save-button save-button-draft-color panel-width">
-            <a class="save-draft-button-font" @click="saveDraft">存草稿</a>
-        </div>
-        <div class="bar-height save-button save-button-save-color panel-width">
-            <a class="save-button-font" @click="save">保存并返回</a>
-        </div>
+        <Row type="flex">
+            <Col span="4" class="text-center bar-item">
+                <a>
+                    <Row>
+                        <Col><i class="button button-icon icon ion-ios-keypad-outline"></i></Col>
+                        <Col>评估</Col>
+                    </Row>
+                </a>
+            </Col>
+            <Col span="4" class="text-center bar-item">
+                <a>
+                    <Row>
+                        <Col><i class="button button-icon icon ion-ios-chatboxes-outline"></i></Col>
+                        <Col>看自评</Col>
+                    </Row>
+                </a>
+            </Col>
+            <Col span="4" class="text-center bar-item">
+                <a>
+                    <Row>
+                        <Col><i class="button button-icon icon ion-android-people"></i></Col>
+                        <Col>看周边</Col>
+                    </Row>
+                </a>
+            </Col>
+            <Col span="6" class="text-center text-nowarp ct-btn btn-draft">
+                <a class="text-white" @click="saveDraft">
+                   存草稿
+                </a>
+            </Col>
+            <Col span="6" class="text-center text-nowrap ct-btn btn-save">
+                <a class="text-white" @click="save">
+                    保存并返回
+                </a>
+            </Col>
+        </Row>
     </div>
 </template>
 <script>
@@ -44,6 +50,38 @@
 <style scoped>
 .bar-height{
     height:1.3rem;
+    padding: 0;
+    background-color: #fff;
+    border-top: 1px solid #eee;
+    border-bottom: 1px solid #eee;
+}
+.bar-item {
+    padding-top: 0.15rem;
+    padding-bottom: 0.15rem;
+}
+.ivu-row-flex {
+    width: 100%;
+}
+.text-white {
+    color: white;
+}
+.text-black {
+    color: black;
+}
+.ct-btn {
+    line-height: 1.3rem;
+}
+.btn-draft {
+    background-color: #33CC33
+}
+.btn-draft a {
+    font-size: 0.3rem;
+}
+.btn-save {
+    background-color: #0066FF;
+}
+.btn-save a {
+    font-size: 0.26rem;
 }
 .panel-width{
 	width:20%;
@@ -54,23 +92,5 @@
 .panel-content{
 	text-align:center;
 	font-size:0.3rem;
-}
-.save-button{
-	text-align: center; 
-	line-height:1.3rem;
-}
-.save-draft-button-font{
-	color: white; 
-	font-size: 0.3rem;
-}
-.save-button-font{
-	color: white; 
-	font-size: 0.26rem;
-}
-.save-button-draft-color{
-	background: #33CC33;
-}
-.save-button-save-color{
-	background: #0066FF;
 }
 </style>
